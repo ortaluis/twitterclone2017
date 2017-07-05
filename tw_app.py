7e7c1572e751cb59ac78a22e5892be5cfcd5a9c6
ortaluis <l13111988>
2017-07-05 21:28:33 +0200

    delete likes post add followings

--- tw_app.py
+++ tw_app.py
@@ -1,16 +1,17 @@
 from pymongo import MongoClient
 from datetime import datetime
+from bson.objectid import ObjectId
 
 client = MongoClient('mongodb://localhost:27017/')
 
 user_id = "19828717"
 userfoll1 = "19828718"
 userfoll2 = "19828719"
+id_tweet = "595ac1c65452123a2cbb5596"
+tweet__post = "Hello World 5"
 
-tweet__post = "Hello World 4"
 
-
-def tweetpost (userid, tweepost):
+def tweetpost(userid, tweepost):
     if len(tweepost) < 140:
         db = client.tweets
         tweet = db.tweets.insert_one(
@@ -26,7 +27,11 @@ def tweetpost (userid, tweepost):
         print ("Tweet should be not longer than 140 characters")
 
 
-#tweetpost(user_id, tweet__post)
+def deletedtweet(userid, idtweet):
+    db = client.tweets
+    add = db.tweets.find_one_and_delete({'_id': ObjectId(idtweet), "user": userid})
+    print ("Tweet deleted")
+    return
 
 
 def getusertimeline(userid):
@@ -36,7 +41,6 @@ def getusertimeline(userid):
         print(document)
     return
 
-#getusertimeline(user_id)
 
 def newuser (userid):
     db0 = client.followers
@@ -62,8 +66,6 @@ def newuser (userid):
     )
     return
 
-#newuser(user_id)
-
 
 def addfollower(userid, useridfollower):
     db = client.followers
@@ -83,12 +85,32 @@ def addfollowing(userid, useridfollowing):
     return
 
 
-def addlike(userid, objidtweet):
-    db = client.likes
-    add = db.likes.find_one_and_update({'user': userid}, {'$push': {'likes': {"tweetliked": objidtweet}}})
+def delfollowing(userid, useridfoll):
+    db = client.followings
+    remove = db.followings.find_one_and_update({'user': userid}, {'$pull': {'followings': {"following": useridfoll}}})
+    print(useridfoll + " was unfollowed")
     return
 
+
+def addlike(userid, idtweet):
+    db = client.likes
+    add = db.likes.find_one_and_update({'user': userid}, {'$push': {'likes': {"tweetliked": idtweet}}})
+    return
+
+
+def dellike(userid, idtweet):
+    db = client.likes
+    remove = db.likes.find_one_and_delete({"user": userid}, {'$pull': {'likes': {"tweetliked": idtweet}}})
+    print ("Tweet deleted from likes")
+    return
+
+#deletedtweet(user_id, id_tweet)
+#tweetpost(user_id, tweet__post)
+#delfollowing(user_id, userfoll1)
+#dellike(user_id, id_tweet)
 #addfollower(user_id, userfoll1)
 #addfollower(user_id, userfoll2)
-
+#addfollowing(user_id, userfoll1)
+#newuser(user_id)
+#getusertimeline(user_id)
 
