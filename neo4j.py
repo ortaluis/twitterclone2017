7394de5ccb93f2e5d578f1d3bbd512ae31b00929
wagdi <wagdimoqbil@gmail.com>
2017-07-08 18:02:23 +0200

    neo4j

--- neo4j.py
+++ neo4j.py
@@ -1,57 +1,26 @@
 #from neo4jrestclient.client import GraphDatabase
 from py2neo import Graph, Node, Relationship
 from pymongo import MongoClient
-
-client = MongoClient('mongodb://localhost:27017/')
-
-
-
-
-
-#setup connection
+import tw_app
 
 graph = Graph("http://neo4j:123123@localhost:7474/db/data/")
 
+
 graph.delete_all()
+client = MongoClient('mongodb://localhost:27017/')
 
-w = Node("Person", name=" Wagdi ")
-graph.create(w)
+user1 = "olga"
+user2 = "mohammed"
+user3 = "luis"
 
-l = Node("Person", name="Luis")
-graph.create(l)
-
-o = Node("Person", name="Olga")
+o = Node("User", user=user1)
 graph.create(o)
 
+g = Node("User", user=user2)
+graph.create(o)
 
-
-alice = Node("Person", name="Alice")
-bob = Node("Person", name="Bob")
-
-alice_knows_bob = Relationship(alice, "KNOWS", bob)
-graph.create(alice_knows_bob)
-
-graph.run("CREATE (c:Person {name:{N}}) RETURN c", {"N": "Carol"})
-for record in graph.run("CREATE (d:Person {name:'Dave'}) RETURN d"):
-    print(record)
-
-for record in graph.run("MATCH (p:Person) RETURN p.name AS name"):
-    print(record[0])
-
-#for record in graph.run("MATCH (p:Person) RETURN p.name AS name"):
-  #  print(record.name)
-
-graph.create(Relationship(w, "friend", l))
-graph.create(Relationship(w, "friend", o))
-graph.create(Relationship(l, "friend", w))
--------------------------------------------
-
-
-
-
-
-
-
+following = Relationship(o,"follows", g)
+graph.create(following)
 
 
 
