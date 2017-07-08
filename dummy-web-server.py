49c994b2cac7ee28213d97ad50ac3dd6f68bb1cc
ortaluis <l13111988>
2017-07-08 16:16:11 +0200

    delete likes post add followings fixed, counters. Olga's code

--- dummy-web-server.py
+++ dummy-web-server.py
@@ -15,6 +15,7 @@ Send a POST request::
     curl -d "foo=bar&bin=baz" http://localhost
 
 """
+#from pymongo import MongoClient
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from urlparse import urlparse, parse_qs
 import SocketServer
@@ -23,12 +24,14 @@ import redis
 class S(BaseHTTPRequestHandler):
 
     r_server = redis.Redis('localhost')
+ #   client = MongoClient()
 
     def _set_headers(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
+    #and here are what a python web-server must do after someone write a page name in the browser
     def do_GET(self):
         #Response auf brower request
         self._set_headers()
@@ -37,18 +40,30 @@ class S(BaseHTTPRequestHandler):
 
         #What to do if a user enter in a browser just: http://localhost
         if self.path == "/":
+
+            #self.r_server.delete('counter')
+
+             # 2.you get them from redis..but wit: where did we defined "last-usermae?"
             last_username = self.r_server.get('last_username')
-            numberOfUsers = self.r_server.get('counter')
             #resultNumberOfUsers = self.r_server.hget('usersNumber', 'numberOfUsers')
 
-            print 'Test: %s' + numberOfUsers
+           #1. defined by you..
+            numberOfUsers ="0"
+            
+            if self.r_server.exists('counter'):
+                numberOfUsers = self.r_server.get('counter')            
+                print 'Test:' + numberOfUsers
 
             if not last_username:
                 last_username = "EMPTY"
 
+            #in order to post stuff on you page in html you can get them from..
+
+            # so if you enter to your main page you need to open a html page called "index.html"
             with open('index.html', 'r') as myfile:
+                #and replace the 2 variables that this page is wating for: (both you get from redis)
                 data=myfile.read().replace('#last_username#',last_username)
-                #data=myfile.read().replace('#usersNumber#',numberOfUsers)
+                data=data.replace('#usersNumber#',numberOfUsers)
                 self.wfile.write(data)
 
         elif self.path == "/help":
@@ -56,6 +71,10 @@ class S(BaseHTTPRequestHandler):
 
         elif self.path == "/sign-up":
             last_username = self.r_server.get('last_username')
+            olga_password = self.r_server.hget('users', "olga")
+
+            if olga_password == "1234":
+               print 'right password'
 
             if not last_username:
                 last_username = "EMPTY"
@@ -70,40 +89,61 @@ class S(BaseHTTPRequestHandler):
 
     def do_HEAD(self):
         self._set_headers()
-        
+
+    # so here are all your responess to..
     def do_POST(self):
-        # what to do with the submitted data from the user
+        # ....what to do with the submitted data from the user (in those forms that appears inside the html pages)
         self._set_headers()
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        self.r_server.set('counter', 1)
 
         #If the data belongs to a form with this tag.. (that was defined in index.html- the data of do_GET)    
         if(self.path == "/submit-exist"):
             post_data = self.rfile.read(content_length) # <--- Gets the data itself
             parsed_data = parse_qs(post_data)
 
+            if (parsed_data['password'][0] != self.r_server.hget('users',parsed_data['username'][0])):
+                with open('username-NOTreceived.html', 'r') as myfile:
+                    data=myfile.read()
+                    self.wfile.write(data)
+                return       
+             #here you can add stuff to my-twiiter page..
             with open('my-twitter.html', 'r') as myfile:
                 data=myfile.read()
                 self.wfile.write(data)
 
 
+       #submit-username is not a page in html but a form that is found in a html page called..sign-up.html
         elif(self.path == "/submit-username"):
             post_data = self.rfile.read(content_length) # <--- Gets the data itself
             parsed_data = parse_qs(post_data)
 
+            if (parsed_data['password'][0] != parsed_data['repeat-password'][0]):
+                with open('username-NOTreceived.html', 'r') as myfile:
+                    data=myfile.read()
+                    self.wfile.write(data)
+                return       
+
+            #like this example: you can set stuff in redis
+            #'here!!' we defined a variable that we want later to use in a page in html..
             self.r_server.set('last_username', parsed_data['username'][0])
             self.r_server.set('last_password', parsed_data['password'][0])
 
             self.r_server.set(parsed_data['username'][0],parsed_data['password'][0])
-            print 'previous set the value: ' + self.r_server.get(parsed_data['username'][0])
+            print 'previous key set the value: ' + self.r_server.get(parsed_data['username'][0])
 
             self.r_server.hset('users', parsed_data['username'][0],parsed_data['password'][0] )
             print 'the value for this key in the hash is %s:'% self.r_server.hget('users',parsed_data['username'][0] )
 
-            self.r_server.incr('counter',1)
+            
+            if(not self.r_server.exists('counter')):
+                self.r_server.set('counter',1)
+            else:
+                self.r_server.incr('counter',1)
 
             with open('username-received.html', 'r') as myfile:
                 data=myfile.read()
+                #you see here: variables username and password are replaced in a page called "username-received.html" with some variables
+                # that are read from a form: what a form? see now..
                 data=data.replace('#username#', parsed_data['username'][0])
                 data=data.replace('#password#', parsed_data['password'][0])
                 self.wfile.write(data)
@@ -113,9 +153,6 @@ class S(BaseHTTPRequestHandler):
             post_data = self.rfile.read(content_length) # <--- Gets the data itself
             parsed_data = parse_qs(post_data)
 
-            #self.numberOfUsers += 1
-            #self.r_server.hset('usersNumber', 'numberOfUsers' ,self.numberOfUsers)
-            #print 'the number of users is %s:'% self.r_server.hget('usersNumber', 'numberOfUsers')
 
             with open('login-received.html', 'r') as myfile:
                 data=myfile.read()
--- tw_app.py
+++ tw_app.py
@@ -1,22 +1,30 @@
 from pymongo import MongoClient
 from datetime import datetime
 from bson.objectid import ObjectId
+from bson.code import Code
+import pprint
+
 
 client = MongoClient('mongodb://localhost:27017/')
 
 user_id = "19828717"
 userfoll1 = "19828718"
 userfoll2 = "19828719"
-id_tweet = "595ac1c65452123a2cbb5596"
-tweet__post = "Hello World 5"
+userfoll3 = "19828720"
+userfoll4 = "19828721"
+userfoll5 = "19828722"
+userfoll6 = "19828723"
+id_tweet = "595e48ce77a3f2c6f85e6c6f"
+tweet__post = "Hello World 7"
 
+# like this???
 # Tweets
 
 def tweetpost(userid, tweepost):
     if len(tweepost) < 140:
         db = client.tweets
-        tweet = db.tweets.insert_one(
-            {"user": userid,
+        tweet = db.tweets.insert_one({
+            "user": userid,
             "tweet_post": tweepost,
             "rt": 0,
             "reply": 0,
@@ -36,6 +44,8 @@ def deletedtweet(userid, idtweet):
     return
 
 
+# Timeline
+
 def getusertimeline(userid):
     db = client.tweets
     cursor = db.tweets.find({"user": userid}).sort("date", -1)
@@ -45,9 +55,10 @@ def getusertimeline(userid):
 
 # Following
 
+
 def addfollowing(userid, useridfollowing):
     db = client.followings
-    if db.followings.find({"user": userid, "following": useridfollowing}):
+    if db.followings.find_one({"user": userid, "following": useridfollowing}):
         print "User is already followed"
     else:
         add = db.followings.insert_one({
@@ -69,7 +80,8 @@ def delfollowing(userid, useridfoll):
 
 def addfollower(userid, useridfollower):
     db = client.followers
-    if client.followers.find({"user": userid, "follower": useridfollower}):
+    if client.followers.find_one({"user": userid, "follower": useridfollower}):
+        print "Follower already exists"
     else:
         add = db.followers.insert_one({
             "user": userid,
@@ -82,7 +94,7 @@ def addfollower(userid, useridfollower):
 
 def addlike(userid, idtweet):
     db = client.likes
-    if db.likes.find({"user": userid, "tweetliked": idtweet}):
+    if db.likes.find_one({"user": userid, "tweetliked": idtweet}):
         print "Tweet already added"
     else:
         add = db.likes.insert_one({"user": userid, "tweetliked": idtweet})
@@ -91,7 +103,7 @@ def addlike(userid, idtweet):
 
 def dellike(userid, idtweet):
     db = client.likes
-    remove = db.likes.find_one_and_delete({"user": userid, "tweetliked": idtweet})
+    remove = db.likes.find_one_and_delete({"user": userid}, {"tweetliked": idtweet})
     print ("Tweet deleted from likes")
     return
 
@@ -121,7 +133,17 @@ def countlikes(userid):
     clikes = db.likes.find({"user": userid}).count()
     return clikes
 
+# MapReduce
 
+
+def getfollowins(userid):
+    db = client.followings
+    result = db.followings.agregate({ '$group:' { _id: "user", "followings": { $sum : 1} } })
+
+
+    return
+
+getfollowins(user_id)
 
 
 
@@ -135,8 +157,12 @@ def countlikes(userid):
 #dellike(user_id, id_tweet)
 #addfollower(user_id, userfoll1)
 #addfollower(user_id, userfoll2)
-#addfollowing(user_id, userfoll1)
+#addfollowing(user_id, userfoll2)
+#addfollowing(user_id, userfoll3)
+#addfollowing(user_id, userfoll4)
+#addfollowing(user_id, userfoll5)
+#addfollowing(user_id, userfoll6)
 #newuser(user_id)
 #getusertimeline(user_id)
-addlike(user_id, id_tweet)
-
+#addlike(user_id, id_tweet)
+#getfollowing(user_id)
